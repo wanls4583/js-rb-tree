67be86c9ff7cc21e2234c3b54f38acc51be950cd
wanls4583 <409051987@qq.com>
2018-09-21 23:32:50 +0800

    codding

--- src/rb_tree.js
+++ src/rb_tree.js
@@ -18,6 +18,12 @@ class RBTree {
     constructor() {
         this.root = null;
     }
+    /**
+     * 插入节点
+     * @param  {[type]} key  节点的key
+     * @param  {[type]} data 节点的数据
+     * @return {Boolean}     插入是否成功
+     */
     insert(key, data) {
         var node = new RBNode(key, data);
         if (!this.root) {
@@ -28,9 +34,32 @@ class RBTree {
         return this._insert(this.root, node);
     }
     /**
+     * 删除节点
+     * @param  {[type]}	 key 需要删除的节点的key
+     * @return {RBNode}     被删除后的点
+     */
+    delete(key) {
+        var result = this._delete(this.root, key);
+        if (result.pre) {
+            result.pre.next = result.next;
+        }
+        if (result.next) {
+            result.next.pre = result.pre;
+        }
+        return result;
+    }
+    /**
+     * 查找节点
+     * @param  {[type]}  key 需要查找的节点的key
+     * @return {RBNode}     查找结果
+     */
+    search(key) {
+        return this._search(this.root, key);
+    }
+    /**
      * 插入节点
-     * @param  {AVLNode} root 子树的根节点
-     * @param  {AVLNode} node 待插入的节点
+     * @param  {RBNode} root 子树的根节点
+     * @param  {RBNode} node 待插入的节点
      * @return {Boolean}      插入是否成功
      */
     _insert(root, node) {
@@ -41,7 +70,7 @@ class RBTree {
                 if (!this._insert(root.lChild, node)) {
                     return false;
                 }
-                this._checkBalance(root);
+                this._insertBalance(root);
             } else { //插入叶子节点
                 root.lChild = node;
                 node.pNode = root;
@@ -51,7 +80,7 @@ class RBTree {
                 if (!this._insert(root.rChild, node)) {
                     return false;
                 }
-                this._checkBalance(root);
+                this._insertBalance(root);
             } else { //插入叶子节点
                 root.rChild = node;
                 node.pNode = root;
@@ -70,15 +99,116 @@ class RBTree {
 
         return true;
     }
-    //右旋转
-    _rRotate(node) {
+    /**
+     * 删除节点
+     * @param  {RBNode} root 	子树的根节点 
+     * @param  {[type]}	 key  	待删除的节点的key
+     * @return {Boolean}      	是否删除成功
+     */
+    _delete(root, key) {
+        if (!root) {
+            return false;
+        }
+        if (root.key == key) {
+            if (!root.lChild && !root.rChild) { //叶子节点，直接删除
+                if (this.root == root) {
+                    this.root = null;
+                } else {
+                    if (root.color == 0) { //待删除节点为黑色，需要调整树
+                        this._deleteBalance(root);
+                    }
+                    if (root.pNode.lChild == root) {
+                        root.pNode.lChild = null;
+                    } else {
+                        root.pNode.rChild = null;
+                    }
+                    this._setHeight(root.pNode);
+                }
+                return root;
+            } else if (!root.lChild || !root.rChild) { //没有右子树或者没有左子树
+                var child = root.lChild || root.rChild;
+                if (this.root == root) {
+                    this.root = child;
+                    this.root.color = 0;
+                    this.root.pNode = null;
+                } else {
+                    if (root.color == 0 && child.color == 0) { //待删除节点为黑色，且其子节点也为黑色，需要调整树
+                        this._deleteBalance(root);
+                    } else if (root.color == 1 && child.color == 1) {
+                        child.color = 0;
+                    }
+                    if (root.pNode.lChild == root) {
+                        root.pNode.lChild = child;
+                    } else {
+                        root.pNode.rChild = child;
+                    }
+                    child.pNode = root.pNode;
+                    this._setHeight(root.pNode);
+                }
+                return root;
+            } else { //用左子树上最大的节点代替root
+                var rChild = root.lChild.rChild;
+                var lChild = root.lChild;
+                while (rChild && rChild.rChild) {
+                    rChild = rChild.rChild;
+                }
+                if (!rChild) {
+                    rChild = lChild;
+                }
+                //交换root和lChild
+                this._change(root, rChild);
+                //保证删除的节点没有左子树，或者没有右子树，用来递归更新所有需要更新高度的节点
+                var result = this._delete(root.lChild, key);
+                this._setHeight(rChild);
+                return result;
+            }
+        } else if (root.key > key) { //在左子树上递归删除
+            var result = this._delete(root.lChild, key);
+            if (!result) {
+                return false;
+            }
+            return result;
+        } else { //在右子树上删除
+            var result = this._delete(root.rChild, key);
+            if (!result) {
+                return false;
+            }
+            return result;
+        }
+    }
+    /**
+     * 搜索节点
+     * @param  {RBNode} root   子树的根节点 
+     * @param  {[type]}  key    待查找的节点的key
+     * @return {RBNode}        查找结果
+     */
+    _search(root, key) {
+        if (!root) {
+            return false;
+        }
+        if (root.key == key) {
+            return root;
+        } else if (root.key > key) {
+            return this._search(root.lChild, key);
+        } else {
+            return this._search(root.rChild, key);
+        }
+    }
+    /**
+     * 右旋转
+     * @param  {RBNdde}	 node          需要旋转的节点
+     * @param  {Boolean} ifChangeColor 是否需要更改节点颜色
+     */
+    _rRotate(node, ifChangeColor) {
         var lc = node.lChild;
         lc.pNode = node.pNode;
         node.lChild = lc.rChild;
         lc.rChild && (lc.rChild.pNode = node);
         lc.rChild = node;
-        node.color = 1;
-        lc.color = 0;
+        if (ifChangeColor) {
+            node.color = 1;
+            lc.color = 0;
+        }
         if (node == this.root) {
             this.root = lc;
         } else {
@@ -90,15 +220,21 @@ class RBTree {
         }
         node.pNode = lc;
     }
-    //左选转
-    _lRotate(node) {
+    /**
+     * 左旋转
+     * @param  {RBNdde}	 node          需要旋转的节点
+     * @param  {Boolean} ifChangeColor 是否需要更改节点颜色
+     */
+    _lRotate(node, ifChangeColor) {
         var rc = node.rChild;
         rc.pNode = node.pNode;
         node.rChild = rc.lChild;
         rc.lChild && (rc.lChild.pNode = node);
         rc.lChild = node;
-        node.color = 1;
-        rc.color = 0;
+        if (ifChangeColor) {
+            node.color = 1;
+            rc.color = 0;
+        }
         if (node == this.root) {
             this.root = rc;
         } else {
@@ -113,31 +249,19 @@ class RBTree {
     //先左选转（不需要变色），再右选转
     _lrRotate(node) {
         //先左旋转左子节点
-        var rc = node.lChild.rChild;
-        node.lChild.rChild = rc.lChild;
-        rc.lChild && (rc.lChild.pNode = node.lChild);
-        rc.lChild = node.lChild;
-        node.lChild.pNode = rc;
-        node.lChild = rc;
-        rc.pNode = node;
+        this._lRotate(node.lChild);
         //右旋转
-        this._rRotate(node);
+        this._rRotate(node, true);
     }
     //先右旋转（不需要变色），再左旋转
     _rlRotate(node) {
         //先右旋转右子节点
-        var lc = node.rChild.lChild;
-        node.rChild.lChild = lc.rChild;
-        lc.rChild && (lc.rChild.pNode = node.rChild);
-        lc.rChild = node.rChild;
-        node.rChild.pNode = lc;
-        node.rChild = lc;
-        lc.pNode = node;
+        this._lRotate(node.rChild);
         //左旋转
-        this._lRotate(node);
+        this._lRotate(node, true);
     }
-    //检查并调整树
-    _checkBalance(node) {
+    //插入后检查并调整树
+    _insertBalance(node) {
         if (node.rChild && node.rChild.color == 1 && (node.rChild.lChild && node.rChild.lChild.color == 1 || node.rChild.rChild && node.rChild.rChild.color == 1)) { //右子树不平衡，需要调整
             var rc = null;
             if (node.lChild && node.lChild.color == 1) { //如果左节点为红节点，不需要旋转，只需要改变颜色
@@ -149,7 +273,7 @@ class RBTree {
             } else {
                 rc = node.rChild;
                 if (node.rChild.rChild && node.rChild.rChild.color == 1) { //左旋转
-                    this._lRotate(node);
+                    this._lRotate(node, true);
                 } else { //先右旋转，再左旋转
                     this._rlRotate(node);
                 }
@@ -168,7 +292,7 @@ class RBTree {
             } else {
                 lc = node.lChild;
                 if (node.lChild.lChild && node.lChild.lChild.color == 1) { //左旋转
-                    this._rRotate(node);
+                    this._rRotate(node, true);
                 } else { //先左旋转，再右旋转
                     this._lrRotate(node);
                 }
@@ -176,7 +300,72 @@ class RBTree {
             //root将变lc的子节点，需要先更新root节点的高度
             this._setHeight(node);
             lc && this._setHeight(lc);
+        } else {
+            this._setHeight(node);
         }
+    }
+    //删除后检查并调整树
+    _deleteBalance(node) {
+        if (!node.pNode) {
+            return;
+        }
+        var pNode = node.pNode;
+        if (pNode.lChild == node) {
+            if (pNode.rChild && pNode.rChild.color == 1) { //兄弟节点为红色
+                pNode.rChild.color = 0;
+                pNode.rChild.lChild && (pNode.rChild.lChild.color = 1);
+                this._lRotate(pNode);
+            } else {
+                if (pNode.rChild && pNode.rChild.rChild && pNode.rChild.rChild.color == 1) { //兄弟节点的右子节点为红色
+                    pNode.color = 0;
+                    pNode.rChild.color = 1;
+                    pNode.rChild.rChild.color = 0;
+                    this._lRotate(pNode);
+                } else if (pNode.rChild && pNode.rChild.lChild && pNode.rChild.lChild.color == 1) { //兄弟节点的左子节点为红色
+                    pNode.color = 0;
+                    this._rRotate(pNode.rChild);
+                    this._lRotate(pNode);
+                } else {
+                    pNode.rChild && (pNode.rChild.color = 1);
+                    this._deleteBalance(pNode);
+                }
+            }
+        } else {
+            if (pNode.lChild && pNode.lChild.color == 1) { //兄弟节点为红色
+                pNode.lChild.color = 0;
+                pNode.lChild.rChild && (pNode.lChild.rChild.color = 1);
+                this._rRotate(pNode);
+            } else {
+                if (pNode.lChild && pNode.lChild.lChild && pNode.lChild.lChild.color == 1) { //兄弟节点的左子节点为红色
+                    pNode.color = 0;
+                    pNode.lChild.color = 1;
+                    pNode.lChild.lChild.color = 0;
+                    this._rRotate(pNode);
+                } else if (pNode.lChild && pNode.lChild.rChild && pNode.lChild.rChild.color == 1) { //兄弟节点的左子节点为红色
+                    pNode.color = 0;
+                    this._lRotate(pNode.lChild);
+                    this._rRotate(pNode);
+                } else {
+                    pNode.lChild && (pNode.lChild.color = 1);
+                    this._deleteBalance(pNode);
+                }
+            }
+        }
+    }
+    /**
+     * 交换两个节点
+     * @param  {RBNode} node1 
+     * @param  {RBNode} node2
+     */
+    _change(node1, node2) {
+        var key = node1.key;
+        var data = node1.data;
+
+        node1.key = node2.key;
+        node1.data = node2.data;
+
+        node2.key = key;
+        node2.data = data;
     }
     //获取树的高度
     _getHeight(node) {
